ccbd2d3c2c746d35928c19ccf8c09acf4fde1605
BinukPinsara <binukpinsara1@gmail.com>
2026-09-04 10:24:58 +0530

    created auth backend

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
-const PORT = 5000;
-
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message: "Server is running!" });
-});
+// Routes
+app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
